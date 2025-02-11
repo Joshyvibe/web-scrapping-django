5a153ac03c56a94086bb72c7a5c2f8f547e6db3f
Obinna Akpulonu <akpulonujoseph@gmail.com>
2025-02-11 10:47:49 +0100

    Update views.py

--- webscrapping/core/views.py
+++ webscrapping/core/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 
-
-
 def get_content(product):
     USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
     LANGUAGE = "en-US,en;q=0.5"
